1ab68ffb4907d9960a86bf522c6054d7915a247c
LuisSoraca27 <luisenriquesoraca27@gmail.com>
2022-08-05 23:24:14 -0500

    quinto commit

--- src/pages/ProductsDetail.jsx
+++ src/pages/ProductsDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { getProductsThunk } from '../store/slices/Products.slice';
@@ -10,6 +11,7 @@ const ProductsDetail = () => {
     const products = useSelector(state => state.products);
     const [productDetail, setProductDetail] = useState({});
     const [suggestedProduct, setSuggestedProduct] = useState([]);
+    const [counter, setCounter] = useState(1)
 
     const { id } = useParams();
     const dispatch = useDispatch();
@@ -42,41 +44,74 @@ const ProductsDetail = () => {
                 <p>{productDetail?.title}</p>
             </div>
 
-            <div className='container-carrusel'>
-                <Carousel className='carrusel'>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100 carrusel-img"
-                            src={productDetail?.productImgs?.[0]}
-                            alt="First slide "
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100 carrusel-img"
-                            src={productDetail?.productImgs?.[1]}
-                            alt="Second slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100 carrusel-img"
-                            src={productDetail?.productImgs?.[2]}
-                            alt="Third slide"
-                        />
-                    </Carousel.Item>
-                </Carousel>
+            <div className='container'>
+                <div className='container-carrusel'>
+                    <Carousel className='carrusel'>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 carrusel-img"
+                                src={productDetail?.productImgs?.[0]}
+                                alt="First slide "
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 carrusel-img"
+                                src={productDetail?.productImgs?.[1]}
+                                alt="Second slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 carrusel-img"
+                                src={productDetail?.productImgs?.[2]}
+                                alt="Third slide"
+                            />
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+                <div className='container-description'>
+                    <h2>{productDetail?.title}</h2>
+                    <div className='description'>
+                        <p>{productDetail?.description}</p>
+                    </div>
+                    <div className='price-option'>
+                        <div>
+                            <h6>price:</h6>
+                            <h5>{'$ ' + productDetail?.price}</h5>
+                        </div>
+                        <div>
+                            <h6>Quantity</h6>
+                            <button>-</button>
+                            <p>{counter}</p>
+                            <button>+</button>
+                        </div>
+                    </div>
+                    <button className='buttom-cart'>Add to cart</button>
+                </div>
             </div>
-
-            {/*   <div>
+            <div className='card-products'>
                 {
                     suggestedProduct.map(product => (
-                        <h3 key={product.id} onClick={() => navigate(`/product/${product.id}`)}>
-                            {product.title}
-                        </h3>
+                        <div className="product" key={product.id} onClick={() => {
+                            navigate(`/product/${product.id}`)
+                            window.scrollTo(0, 0);
+                        }}>
+                            <div className='imgProduct'>
+                                <img src={product.productImgs} alt="" />
+                            </div>
+                            <div style={{ height: '80px' }}>
+                                <h5>{product.title}</h5>
+                            </div>
+                            <div className='price'>
+                                <p>price</p>
+                                <h5>{product.price}</h5>
+                                <button><i class="fa-solid fa-cart-shopping"></i></button>
+                            </div>
+                        </div>
                     ))
                 }
-            </div> */}
+            </div>
         </aside>
     );
 };
